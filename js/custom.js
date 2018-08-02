7b7916fc6dc3c980f31e3d39d423958f6348d575
steven garner <stevengarner3@gmail.com>
2018-08-02 03:40:11 +0100

    fixed navigation, fixed and otimized images for web.

--- js/custom.js
+++ js/custom.js
@@ -140,30 +140,9 @@ $(function () {
 
 // Close mobile menu on click
 $(function(){
-    
+
     $(".navbar-collapse ul li a").on("click touch", function(){
-       
+
         $(".navbar-toggle").click();
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
